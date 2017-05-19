a57d61abc67664bef46ccc044594d43ee87ecd46
Marco Nie <nie11kun@outlook.com>
2017-05-19 08:50:56 +0800

    update

--- Gesturizer/ViewController.swift
+++ Gesturizer/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var doubleTapRecognizer: UITapGestureRecognizer!
+    
+    @IBOutlet var singleTapRecognizer: UITapGestureRecognizer!
+    
+    @IBAction func singleTap(_ sender: UITapGestureRecognizer) {
+        self.showGestureName(name: "Tap")
+    }
+    
+    @IBAction func doubleTap(_ sender: UITapGestureRecognizer) {
+        self.showGestureName(name: "Double Tap")
+    }
+    
+    @IBAction func pinch(_ sender: UIPinchGestureRecognizer) {
+        if sender.state == .ended {
+            self.showGestureName(name: "Pinch")
+        }
+    }
+    
+    @IBAction func rotation(_ sender: UIRotationGestureRecognizer) {
+        if sender.state == .ended {
+            self.showGestureName(name: "Rotation")
+        }
+    }
+    
+    @IBAction func swipe(_ sender: UISwipeGestureRecognizer) {
+        if sender.direction == .right {
+            self.showGestureName(name: "Right Swipe")
+        }
+    }
+    
+    @IBOutlet weak var gesturizerName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        singleTapRecognizer.require(toFail: doubleTapRecognizer)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +53,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func showGestureName(name: String) {
+        gesturizerName.text = name
+        UIView.animate(withDuration: 1.0,
+                       animations: { self.gesturizerName.alpha = 1.0 },
+                       completion: { _ in
+                        UIView.animate(withDuration: 1.0, animations: { self.gesturizerName.alpha = 0.0 })
+        })
+    }
+    
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            self.showGestureName(name: "Shake")
+        }
+    }
+    
 }
 
